5fb8465a0fe6fda710be8f30bde0e51fa8886399
rogerpt32 <rogerpt32@gmail.com>
2018-11-06 13:56:29 +0100

    add: pid msort

--- lab5/msort.erl
+++ lab5/msort.erl
@@ -16,3 +16,21 @@ ms([A]) ->[A];
 ms(L) ->
   {L1, L2} = sep(L, length(L)div 2),
   merge(ms(L1),ms(L2)).
+
+rcvp(Pid) ->
+  receive
+    {Pid, L} -> L
+  end.
+
+pms(L) ->
+  Pid = spawn(msort, p_ms, [self(), L]),
+  rcvp(Pid).
+
+p_ms(Pid, L) when length(L) < 100 -> Pid ! {self(), ms(L)};
+p_ms(Pid, L) ->
+  {Lleft, Lright} = sep(L, length(L)div 2),
+  Pid1 = spawn(msort, p_ms, [self(), Lleft]),
+  Pid2 = spawn(msort, p_ms, [self(), Lright]),
+  L1 = rcvp(Pid1),
+  L2 = rcvp(Pid2),
+  Pid ! {self(),merge(L1,L2)}.
