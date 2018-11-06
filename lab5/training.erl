dffe36f6f115bc44e5fe69e5082238523ac20b3c
miquelalcon <miquel.1996@gmail.com>
2018-11-06 12:28:17 +0100

    training:pyth working

--- lab5/training.erl
+++ lab5/training.erl
@@ -1,1 +1,10 @@
+-module(training).
+-export([pyth/1]).
 
+pyth(N) ->
+    [ {A,B,C} ||
+        A <- lists:seq(1,N),
+        B <- lists:seq(1,N),
+        C <- lists:seq(1,N),
+        A+B+C =< N,
+        A*A+B*B == C*C ].
