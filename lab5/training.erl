d57d6455deb832d0fc2d8593bb1e49e13ee92ee7
rogerpt32 <rogerpt32@gmail.com>
2018-11-06 12:53:19 +0100

    add: training

--- lab5/training.erl
+++ lab5/training.erl
@@ -10,4 +10,14 @@ pyth(N) ->
         A*A+B*B == C*C ].
 
 range(N, N) -> [N];
-range(Min, Max) -> [Min | range(Min+1, Max)].
+range(Min, Max) when Min > Max -> [];
+range(Min, Max) when Min < Max -> [Min | range(Min+1, Max)].
+
+remove_multiples(N, [H|T]) when H rem N == 0 -> remove_multiples(N,T);
+remove_multiples(N, [H|T]) -> [H|remove_multiples(N,T)];
+remove_multiples(_, []) -> [].
+
+sieve([H|T]) -> [H | sieve(remove_multiples(H, T))];
+sieve([]) -> [].
+
+primes(Max) -> sieve(range(2, Max)).
