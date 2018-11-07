50c50fc5d32f07ceab16c0f7d4a34f5d33b5a1ea
miquelalcon <miquel.1996@gmail.com>
2018-11-07 11:41:06 +0100

    pingpong

--- lab5/pingpong.erl
+++ lab5/pingpong.erl
@@ -2,22 +2,24 @@
 -export([start/0, ping/2, pong/0]).
 
 start() ->
-    Pong_PID = spawn(..., pong, []),
-    spawn(..., ping, [3, Pong_PID]).
+    Pong_PID = spawn(pingpong, pong, []),
+    spawn(pingpong, ping, [3, Pong_PID]).
 ping(0, Pong_PID) ->
     Pong_PID ! finished,
     io:format("Ping finished~n", []);
 ping(N, Pong_PID) ->
-    Pong_PID ! {ping, ...},
-    .....
-    .....
-    .....
+    Pong_PID ! {ping, self()},
+    receive
+        pong ->
+            io:format("Ping recieved pong~n", []),
+            ping(N-1, Pong_PID)
+    end.
 pong() ->
     receive
         finished ->
             io:format("Pong finished~n", []);
-        {ping, ...} ->
+        {ping, Pid} ->
             io:format("Pong received ping~n", []),
-            ....
-            ....
+            Pid ! pong,
+            pong()
     end.
